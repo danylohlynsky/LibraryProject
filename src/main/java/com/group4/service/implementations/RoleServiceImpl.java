f1024a988362bd63be0a9981e3f202c09c10662a
Данило Глинський <Danylo.Hlynskyi.PZ.2020@edu.lpnu.ua>
2022-02-11 03:07:51 +0200

    renamed role services to convention

--- src/main/java/com/group4/service/implementations/RoleServiceImpl.java
+++ src/main/java/com/group4/service/implementations/RoleServiceImpl.java
@@ -2,18 +2,18 @@ package com.group4.service.implementations;
 
 import com.group4.dao.interfaces.RoleDAO;
 import com.group4.model.Role;
-import com.group4.service.interfaces.RoleInterface;
+import com.group4.service.interfaces.RoleService;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @NoArgsConstructor
 @Repository
-class RoleService implements RoleInterface {
+class RoleServiceImpl implements RoleService {
     private RoleDAO roleDAO;
 
     @Autowired
-    public RoleService(RoleDAO roleDAO) {
+    public RoleServiceImpl(RoleDAO roleDAO) {
         this.roleDAO = roleDAO;
     }
 
--- src/main/java/com/group4/service/interfaces/RoleService.java
+++ src/main/java/com/group4/service/interfaces/RoleService.java
@@ -2,7 +2,7 @@ package com.group4.service.interfaces;
 
 import com.group4.model.Role;
 
-public interface RoleInterface {
+public interface RoleService {
     void save(Role role);
     void update(Role role);
 }
