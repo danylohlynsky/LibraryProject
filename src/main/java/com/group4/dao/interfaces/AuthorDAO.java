d6d81bad4f3960cd51d0e94a5238c9ee370d6d34
Данило Глинський <Danylo.Hlynskyi.PZ.2020@edu.lpnu.ua>
2022-02-11 08:04:56 +0200

    finding author by full name added

--- src/main/java/com/group4/dao/interfaces/AuthorDAO.java
+++ src/main/java/com/group4/dao/interfaces/AuthorDAO.java
@@ -2,4 +2,6 @@ package com.group4.dao.interfaces;
 
 import com.group4.model.Author;
 
-public interface AuthorDAO extends DAO<Author> {}
+public interface AuthorDAO extends DAO<Author> {
+    Author findByFullName(String[] fullName);
+}
