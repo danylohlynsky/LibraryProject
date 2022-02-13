dac0f10eb3959d67013167e0e41127c0fd00cbcf
Dima Prudius <dimaprudius1995@gmail.com>
2022-02-14 00:34:01 +0200

    added changes to controller users and author also changes UserDAOImpl and added create-author.html and update-author.html

--- src/main/java/com/group4/controller/AuthorController.java
+++ src/main/java/com/group4/controller/AuthorController.java
@@ -1,37 +1,58 @@
 package com.group4.controller;
 
+import com.group4.model.Author;
 import com.group4.service.interfaces.AuthorService;
 import com.group4.service.interfaces.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/authors")
 public class AuthorController {
 
-    private final BookService bookService;
     private final AuthorService authorService;
 
     @Autowired
-    public AuthorController(BookService bookService, AuthorService authorService) {
-        this.bookService = bookService;
+    public AuthorController(AuthorService authorService) {
         this.authorService = authorService;
     }
 
     @GetMapping
-    public String listBooks(Model model) {
+    public String listAuthor(Model model) {
         model.addAttribute("listAuthor", this.authorService.listAuthor());
         return "list-author";
+
+    }
+
+    @GetMapping(value = "/add")
+    public String create(Model model) {
+        model.addAttribute("author", this.authorService.listAuthor());
+        return "create-author";
+    }
+
+    @PostMapping(value = "/add")
+    public String create(@Validated @ModelAttribute("author") Author author, BindingResult result) {
+        if (result.hasErrors()) {
+            return "create-author";
+        }
+        authorService.addAuthor(author);
+        return "redirect:/authors";
     }
 
     @GetMapping("/remove/{id}")
     public RedirectView removeBook(@PathVariable("id") int id) {
         this.authorService.removeAuthor(id);
         return new RedirectView("/authors");
+    }
+
+    @GetMapping("update/{id}")
+    public String update(@PathVariable("id") int id, Model model) {
+        model.addAttribute("author", authorService.getAuthorById(id));
+        return "update-author";
     }
 }
--- src/main/java/com/group4/service/interfaces/UserService.java
+++ src/main/java/com/group4/service/interfaces/UserService.java
@@ -1,17 +1,20 @@
 package com.group4.service.interfaces;
 
+import com.group4.model.Book;
 import com.group4.model.User;
+
 import java.util.List;
 
 public interface UserService {
+    void addUser(User user);
+
     User findById(int id);
 
-    List<User> findAll();
+    List<User> listUser();
 
     void saveUser(User user);
 
     User deleteById(int id);
-
 
 
 }
